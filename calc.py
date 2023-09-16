ee2733a4d86be0d6ddd60b8c858e484125b7f09c
anchit-chandran <anchit97123@gmail.com>
2023-09-16 14:42:39 +0100

    reformat
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- calc.py
+++ calc.py
@@ -21,7 +21,7 @@ entry.grid(
 def insert_char(text):
     current = entry.get()
     entry.delete(0, END)
-    entry.insert(0, current+text)
+    entry.insert(0, current + text)
 
 
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: insert_char("1"))
@@ -33,6 +33,7 @@ button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: insert_char(
 button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: insert_char("7"))
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: insert_char("8"))
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: insert_char("9"))
+button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: insert_char("0"))
 
 button_1.grid(row=1, column=0)
 button_2.grid(row=1, column=1)
@@ -43,79 +44,25 @@ button_6.grid(row=2, column=2)
 button_7.grid(row=3, column=0)
 button_8.grid(row=3, column=1)
 button_9.grid(row=3, column=2)
+button_0.grid(row=4, column=1)
 
-button_clear = Button(
-    root,
-    text="C",
-    padx=40,
-    pady=20,
-)
-button_clear.grid(
-    row=4,
-    column=0,
-)
-button_0 = Button(
-    root,
-    text="0",
-    padx=40,
-    pady=20,
-)
-button_0.grid(
-    row=4,
-    column=1,
-)
-button_equals = Button(
-    root,
-    text="=",
-    padx=40,
-    pady=20,
-)
-button_equals.grid(
-    row=4,
-    column=2,
-)
+def clear_entry():
+    entry.delete(0, END)
 
-button_plus = Button(
-    root,
-    text="+",
-    padx=40,
-    pady=20,
-)
-button_minus = Button(
-    root,
-    text="-",
-    padx=40,
-    pady=20,
-)
-button_multiply = Button(
-    root,
-    text="*",
-    padx=40,
-    pady=20,
-)
-button_divide = Button(
-    root,
-    text="/",
-    padx=40,
-    pady=20,
-)
+button_clear = Button(root, text="C", padx=40, pady=20, command=clear_entry)
+button_equals = Button(root, text="=", padx=40, pady=20,)
 
+button_clear.grid(row=4, column=0)
+button_equals.grid(row=4, column=2)
 
-button_plus.grid(
-    row=1,
-    column=3,
-)
-button_minus.grid(
-    row=2,
-    column=3,
-)
-button_multiply.grid(
-    row=3,
-    column=3,
-)
-button_divide.grid(
-    row=4,
-    column=3,
-)
+button_plus = Button(root, text="+", padx=40, pady=20,)
+button_minus = Button(root, text="-", padx=40, pady=20,)
+button_multiply = Button(root, text="*", padx=40, pady=20,)
+button_divide = Button(root, text="/", padx=40, pady=20,)
+
+button_plus.grid(row=1, column=3)
+button_minus.grid(row=2, column=3)
+button_multiply.grid(row=3, column=3)
+button_divide.grid(row=4, column=3)
 
 root.mainloop()
