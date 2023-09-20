d502030fb56a85490e2e46f2519e34e9525286fc
anchit-chandran <anchit97123@gmail.com>
2023-09-20 20:00:07 +0100

    adds db read
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- wardTrack.py
+++ wardTrack.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import sqlite3
 
 data = [
     {
@@ -34,9 +35,96 @@ class WardApp:
         self.root.geometry(f"{200*len(data)}x400")
         self.root.title("WardTrack 2.0")
 
-        self.draw_pts()
+        if not self.db_exists():
+            self._create_db()
+            self._seed_pts()
 
+        self.draw_pts()
+        
+        debug_button = tk.Button(self.root, text='DEBUG', command=lambda: self.debug())
+        debug_button.grid(row=1, column=0)
+        
         self.root.mainloop()
+    
+    def debug(self)->None:
+        conn = self.connect_to_db()
+        conn.row_factory = sqlite3.Row
+        
+        c = conn.cursor()
+        
+        c.execute("""SELECT * FROM ward_pts""")
+        records = [dict(item) for item in c.fetchall()]
+        print(records)
+        
+        conn.commit()
+        conn.close()
+    
+    def get_all_records(self)->list[tuple]:
+        conn = self.connect_to_db()
+        conn.row_factory = sqlite3.Row
+        
+        c = conn.cursor()
+        
+        c.execute("""SELECT * FROM ward_pts""")
+        records = [dict(item) for item in c.fetchall()]     
+        conn.commit()
+        conn.close()
+        
+        return records
+    
+    def db_exists(self):
+        import os
+        
+        cwd = os.getcwd()
+        
+        db_name = 'ward.db'
+        
+        return os.path.isfile(os.path.join(cwd, db_name))
+        
+    
+    def connect_to_db(self):
+        return sqlite3.connect('ward.db')
+
+    def _create_db(self):
+        conn = sqlite3.connect("ward.db")
+
+        c = conn.cursor()
+
+        c.execute(
+            """CREATE TABLE ward_pts (
+                first_name text,
+                last_name text,
+                age int,
+                sex text,
+                bed int,
+                diagnosis text
+            )"""
+        )
+
+        conn.commit()
+
+        conn.close()
+
+    def _seed_pts(self) -> None:
+        conn = sqlite3.connect("ward.db")
+
+        c = conn.cursor()
+
+        for pt in self.get_all_records():
+            c.execute(
+                """INSERT INTO ward_pts VALUES (:first_name, :last_name, :age, :sex, :bed, :diagnosis )""",
+                {
+                    "first_name": pt["first_name"],
+                    "last_name": pt["last_name"],
+                    "age": pt["age"],
+                    "sex": pt["sex"],
+                    "bed": pt["bed"],
+                    "diagnosis": pt["diagnosis"],
+                },
+            )
+
+        conn.commit()
+        conn.close()
 
     def draw_pts(self) -> None:
         for ix, pt in enumerate(data):
